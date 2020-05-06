665213d49e4a92c2c20d7c2391dea91cca952c8c
Rick Schoenmaker <rickschoenmaker@hotmail.com>
2020-05-06 12:00:23 +0200

    extract allel freq with more than 0.01%

--- VCFconv.py
+++ VCFconv.py
@@ -1,5 +1,18 @@
 import vcf
 
-vcf_reader = vcf.Reader(open('gnomad.exomes.r2.1.1.sites.Y.vcf', 'r'))
-for record in vcf_reader:
-    print(record.INFO)
+vcf_reader = vcf.Reader(open('/home/rick/Documents/Data-intergratie/data/gnomad.exomes.r2.1.1.sites.Y.vcf', 'r'))
+id = []
+freq = []
+try:
+    for record in vcf_reader:
+        number = str(record.INFO['AF']).strip("[]").replace("e-", "")
+        number = float(number)
+        if float(number) >= float(0.01):
+            print(number)
+            id.append(str(record.ID))
+            freq.append(str(record.INFO['AF']).strip("[]"))
+
+except:
+        pass
+        print("faulty")
+print(len(freq))
