b8187cf8cdcea1261cd90c312748c8ff6633bcf7
Rick Schoenmaker <rickschoenmaker@hotmail.com>
2020-06-04 11:12:40 +0200

    Merge branch 'develop' of https://github.com/evandervanwolfswinkel/VCFconversionAPI into develop
     Conflicts:
    	app.py
    	requirements.txt

--- loadDB.py
+++ loadDB.py
@@ -1,17 +1,19 @@
 import os
 import urllib.parse as up
-import psycopg2
+import psycopg2_binary as pg2
+
 import pickle
 import _pickle as cPickle
 
 def main(chromosome):
     conn = connection()
     unpickledlist = retrieveData(conn, chromosome)
+    return unpickledlist
 
 def connection():
     up.uses_netloc.append("postgres")
     url = up.urlparse("postgres://gjeciswx:Ax3-QX8t2FGwNnlEEXh5UaQOPTKjQEtn@dumbo.db.elephantsql.com:5432/gjeciswx")
-    conn = psycopg2.connect(database=url.path[1:],
+    conn = pg2.connect(database=url.path[1:],
                             user=url.username,
                             password=url.password,
                             host=url.hostname,
@@ -34,7 +36,7 @@ def retrieveData(conn, chromosome):
         conn.close()
         print("committed!")
         return unpickledList
-    except (Exception, psycopg2.DatabaseError) as error:
+    except (Exception, pg2.DatabaseError) as error:
             print(error)
     finally:
         if conn is not None:
