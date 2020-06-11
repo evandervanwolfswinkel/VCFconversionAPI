24ced39b551f19479c827ce8bea6b58f5680f2d0
Rick Schoenmaker <rickschoenmaker@hotmail.com>
2020-06-11 19:39:53 +0200

    Merge branch 'develop' of https://github.com/evandervanwolfswinkel/VCFconversionAPI into develop
     Conflicts:
    	app.py
    	requirements.txt

--- VCFconv.py
+++ VCFconv.py
@@ -1,16 +1,32 @@
+# Author: Rick Schoenmaker & Evander Van Wolfswinkel
+# Class: BIN3A
+
+
+
+# import dependencies.
 import vcf
 import pickle
 
+# Import vcf reader to open vcf files.
 vcf_reader = vcf.Reader(open('gnomad.exomes.r2.1.1.sites.Y.vcf', 'r'))
+
+# create a dictionary
 dicts = {}
+
+
 try:
+    # A for loop to looping through all records in the vcf file.
     for record in vcf_reader:
+        # Retrieves the allele frequenties in a string and removes [ ] and e-.
         number = str(record.INFO['AF']).strip("[]").replace("e-", "")
+        # Turns string number into float.
         number = float(number)
+        # Filters the allele frequenties to select only the the ones that appear less than 0.01%
         if float(number) <= float(0.01):
-            #print(number)
+            # Create a dictionary
             dict = {}
-            dict[str(record.POS)] = [str(record.REF),str(record.ALT),
+            # Fills a dictionary with position, reference nucleotide(s), alternative nucleotide(s), allele frequentie and ID
+            dict[str(record.POS)] = [str(record.REF), str(record.ALT),
                                      str(record.INFO['AF']).strip("[]"),
                                      str(record.ID)]
             dicts.update(dict)
@@ -18,9 +34,8 @@ try:
 
 except:
         pass
-        print("faulty")
 
-print(dicts)
+# Create a pickle from the dictionary and save it in the static folder.
 outfile = open('static/X', 'wb')
-pickle.dump(dicts,outfile)
+pickle.dump(dicts, outfile)
 outfile.close()
